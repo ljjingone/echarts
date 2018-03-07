468e51ba2179639e4f7328b00392f4c68e68f9fe
ljjingone <947598275@qq.com>
2018-03-07 12:14:13 +0800

    add router

--- src/main.js
+++ src/main.js
@@ -3,16 +3,32 @@
 import Vue from 'vue'
 import App from './App'
 import axios from 'axios'
-
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 Vue.prototype.$ajax=axios
 
 
 Vue.config.productionTip = false
 
+
+Vue.use(VueRouter)
+const router = new VueRouter({
+	routes:Routes,
+	mode:"history"
+})
+router.beforeEach((to,from,next)=>{
+	
+	if(to.meta.title){
+		document.title=to.meta.title
+		next();
+	}	
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  router
 })
